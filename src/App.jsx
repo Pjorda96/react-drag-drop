dbb1f1745021e4ab96294ced4fb841287bc324ca
pabloPc <pjorda96@gmail.com>
2021-04-07 19:05:44 +0200

    move on cascade

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,19 @@ const App = () => {
       id: "Box-3",
       color: "blue",
       order: 3
+    },
+    {
+      id: "Box-4",
+      color: "orange",
+      order: 4
+    },
+    {
+      id: "Box-5",
+      color: "fuchsia",
+      order: 5
     }
   ]);
-  const [dragId, setDragId] = useState();
+  const [dragId, setDragId] = useState(null);
 
   const handleDrag = (ev) => {
     setDragId(ev.currentTarget.id);
@@ -30,16 +40,43 @@ const App = () => {
     const dragBox = boxes.find(box => box.id === dragId);
     const dropBox = boxes.find(box => box.id === ev.currentTarget.id);
 
+    // indexOf with no order property
     const dragBoxOrder = dragBox.order;
     const dropBoxOrder = dropBox.order;
 
     const newBoxState = boxes.map(box => {
       if (box.id === dragId) {
+        return {
+          ...box,
+          order: dropBoxOrder, // splice if no order property
+        };
+      } else if (
+        dragBoxOrder > dropBoxOrder // if move it to up change between drop to drag
+        && box.order >= dropBoxOrder // greater or equal than drop
+        && box.order < dragBoxOrder // lower than drag (drag handled first)
+      ) {
+        return {
+          ...box,
+          order: box.order + 1,
+        }
+      } else if (
+        dragBoxOrder < dropBoxOrder // if move it to down change between drag to drop
+        && box.order > dragBoxOrder // greater than drag (drag handled first)
+        && box.order <= dropBoxOrder // lower or equal than drop
+      ) {
+        return {
+          ...box,
+          order: box.order - 1,
+        }
+      }
+
+      /*if (box.id === dragId) {
         box.order = dropBoxOrder;
       }
       if (box.id === ev.currentTarget.id) {
         box.order = dragBoxOrder;
-      }
+      }*/
+
       return box;
     });
 
